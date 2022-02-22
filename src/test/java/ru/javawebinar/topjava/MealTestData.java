76d9af80231f80d4e581ada0306096eb1ce331b4
Aleksey Ilin <kayrus@mail.ru>
2022-02-22 22:24:10 +0300

    HW_03 #2 changes: - spring-db.xml : restore username/password use in project - initDB.sql : correct name unique index for Meals table - JdbcMealRepository : correct WHERE for get between date - MealTestData : add comparator for field dateTime

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -4,7 +4,6 @@ import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
 import java.time.Month;
-import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -16,21 +15,17 @@ public class MealTestData {
     public static final int START_MEAL_SEQ = START_SEQ + 3;
     public static final int MEAL_ID = START_MEAL_SEQ;
     public static final Meal meal1 = new Meal(MEAL_ID, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500);
-    public static final Meal meal2 = new Meal(MEAL_ID + 1, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Обед", 1000);
-    public static final Meal meal3 = new Meal(MEAL_ID + 2, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Ужин", 500);
-    public static final Meal meal4 = new Meal(MEAL_ID + 3, LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Еда на граничное значение", 100);
-    public static final Meal meal5 = new Meal(MEAL_ID + 4, LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Завтрак", 1000);
-    public static final Meal meal6 = new Meal(MEAL_ID + 5, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Обед", 500);
-    public static final Meal meal7 = new Meal(MEAL_ID + 6, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Ужин", 410);
+    public static final Meal meal2 = new Meal(MEAL_ID + 1, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000);
+    public static final Meal meal3 = new Meal(MEAL_ID + 2, LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500);
+    public static final Meal meal4 = new Meal(MEAL_ID + 3, LocalDateTime.of(2020, Month.JANUARY, 31, 00, 0), "Еда на граничное значение", 100);
+    public static final Meal meal5 = new Meal(MEAL_ID + 4, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000);
+    public static final Meal meal6 = new Meal(MEAL_ID + 5, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500);
+    public static final Meal meal7 = new Meal(MEAL_ID + 6, LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410);
     public static final int NOT_FOUND = 1000;
     public static final int MEAL_ADMIN_ID = START_MEAL_SEQ + 7;
     public static final Meal mealAlien1 = new Meal(MEAL_ADMIN_ID, LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Завтрак", 500);
-//    public static final Meal MEAL_2_ADMIN = new Meal(MEAL_ADMIN_ID + 1, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500);
 
     public static final Meal mealNotExist = new Meal(NOT_FOUND, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500);
-
-    private static final Comparator<LocalDateTime> truncateSeconds = (a, exp) ->
-            a.isAfter(exp.truncatedTo(ChronoUnit.SECONDS)) ? 0 : 1;
 
     public static Meal getNew() {
         return new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 30), "Завтрак", 500);
@@ -55,7 +50,7 @@ public class MealTestData {
     public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
         assertThat(actual)
                 .usingRecursiveFieldByFieldElementComparatorIgnoringFields("dateTime")
-                .usingComparatorForType(truncateSeconds, LocalDateTime.class)
+                .usingComparator(Comparator.comparing(o -> o.iterator().next().getDateTime()), "dateTime")
                 .isEqualTo(expected);
     }
 }
